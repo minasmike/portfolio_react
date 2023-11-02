50ee9a4af80c6eba52e5192d83e8e5a3e394cc06
minas mike <menelikmichael@gmail.com>
2023-11-02 21:19:24 +0300

    firebase

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -5,12 +5,11 @@ const SkillCard = ({ SkillItem }) => {
   const AltText = SkillItem.AltText;
   const SkillName = SkillItem.SkillName;
   return (
-    <div>
+    
       <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
         <img className="w-20 mx-auto" src={ImgSrc} alt={AltText} />
         <p className="my-4 font-semibold">{SkillName}</p>
       </div>
-    </div>
   );
 };
 
